436f74685a301abc047ea2830473a030902c4ac4
Shane White <Shane.P.White@gmail.com>
2023-05-13 18:05:11 -0500

    Add project files.

---
+++ AdsbDisplay/Worker.cs
@@ -0,0 +1,22 @@
+namespace AdsbDisplay
+{
+    public class Worker : BackgroundService
+    {
+        private readonly ILogger<Worker> _logger;
+
+        public Worker(ILogger<Worker> logger)
+        {
+            _logger = logger;
+        }
+
+        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            //The core loop of the program
+            while (!stoppingToken.IsCancellationRequested)
+            {
+                _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
+                await Task.Delay(1000, stoppingToken);
+            }
+        }
+    }
+}
